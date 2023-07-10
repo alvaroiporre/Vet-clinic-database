debeb6375149347fb82177633eeb2f0b595ad67f
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-10 18:30:12 -0400

    Add data

--- schema.sql
+++ schema.sql
@@ -1,15 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE DATABASE vet_clinic
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-	
 CREATE TABLE animals (
 	id int primary key,
 	name varchar(20) NOT NULL,
