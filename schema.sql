94de9038626fd7a44d951beedb35c141d1c38399
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-10 18:17:46 -0400

    Add animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
 CREATE TABLE animals (
-    name varchar(100)
+	id int primary key,
+	name varchar(20) NOT NULL,
+	date_of_birth date,
+	escape_attempts int,
+	neutered bool,
+	weight_kg real
 );
