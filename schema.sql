24c2e513e30a562d4d65f0ad7c494c3f9129eb42
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-11 17:43:51 -0400

    Fix optional changes

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-	id int primary key,
+	id SERIAL NOT NULL,
 	name varchar(20) NOT NULL,
 	date_of_birth date,
 	escape_attempts int,
 	neutered bool,
-	weight_kg real
+	weight_kg real,
+    PRIMARY KEY(id)
 );
