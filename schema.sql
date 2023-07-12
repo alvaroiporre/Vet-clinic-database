215d7ed6a2609f99f4bdfbba7bf3392c77e333ee
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-12 09:09:17 -0400

    Add schema tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,41 @@ CREATE TABLE animals (
     Add a new column species
 */
 ALTER TABLE animals ADD species varchar(20);
+
+/* Create a table named owners with the following columns:
+    id: integer (set it as autoincremented PRIMARY KEY)
+    full_name: string
+    age: integer */
+CREATE TABLE owners(
+	id serial not null,
+	full_name varchar(60),
+	age int,
+	PRIMARY KEY(id)
+);
+
+/* Create a table named species with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string */
+CREATE TABLE species(
+	id serial not null,
+	name varchar(20),
+	PRIMARY KEY(id)
+);
+
+/* Modify animals table:
+Make sure that id is set as autoincremented PRIMARY KEY
+Remove column species
+Add column species_id which is a foreign key referencing species table
+Add column owner_id which is a foreign key referencing the owners table */
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id int;
+ALTER TABLE animals
+ADD CONSTRAINT FK_SPECIES FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id int;
+ALTER TABLE animals
+ADD CONSTRAINT FK_OWNER FOREIGN KEY(owner_id) REFERENCES owners(id);
